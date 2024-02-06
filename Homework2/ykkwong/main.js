e7045f366319f281591a44b4ab94362425cc92ae
Kris Wong <37485546+SerpentSnek@users.noreply.github.com>
2024-02-05 22:24:27 -0800

    done with parallel plot
    now its time for the two other charts!!!

--- Homework2/ykkwong/main.js
+++ Homework2/ykkwong/main.js
@@ -2,87 +2,70 @@ let abFilter = 25
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-let parallelLeft = 0, scatterTop = 0;
-let parallelMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    parallelWidth = 400 - parallelMargin.left - parallelMargin.right,
-    parallelHeight = 350 - parallelMargin.top - parallelMargin.bottom;
+let parallelLeft = 0, parallelTop = 0;
+let parallelMargin = {top: 40, right: 0, bottom: 10, left: 0},
+    parallelWidth = width,
+    parallelHeight = 350 - parallelMargin.top;
 
 let distrLeft = 400, distrTop = 0;
-let distrMargin = {top: 10, right: 30, bottom: 30, left: 60},
+let distrMargin = {top: 10, right: 30, bottom: 30, left: 30},
     distrWidth = 400 - distrMargin.left - distrMargin.right,
     distrHeight = 350 - distrMargin.top - distrMargin.bottom;
 
-let teamLeft = 0, teamTop = 400;
-let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    teamWidth = width - teamMargin.left - teamMargin.right,
-    teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
-
-// helper method to get all unique values of keys:
-// https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
-// const unique = [...new Set(data.map(item => item.group))];    
-
-
-  // var unique = [];
-  // var distinct = [];
-  // for( let i = 0; i < data.length; i++ ) {
-  //   if( !unique[data[i].age]){
-  //     distinct.push(data[i].age);
-  //     unique[data[i].age] = 1;
-  //   }
-  // }
-  // return distinct;
+// let teamLeft = 0, teamTop = 400;
+// let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
+//     teamWidth = width - teamMargin.left - teamMargin.right,
+//     teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
 
 // get the data
 d3.csv("ds_salaries.csv").then(data => {
-  // console.log("rawData", data);
-  // console.log(Object.keys(data[0]));
-    
-  // shove all the data vis here
-  // do parallel coordinates diagram for overview
-  // make the overview one quarter of screen space
-  // Enumerate all keys
-  // for (i = 0; i < data.length; i++) {
-  //   for (key = 0; key < data[0].length; key++) {
-  //     Object.keys(data[i])
-  //   }
-  // }
   
+  const svg = d3.select("svg")
+  // parallel plot
+  
+  const g1 = svg.append("g")
+                .attr("width", parallelWidth + parallelMargin.left + parallelMargin.right)
+                .attr("height", parallelHeight + parallelMargin.top + parallelMargin.bottom)
+                .attr("transform", `translate(${parallelMargin.left}, ${parallelMargin.top})`)
   // https://d3-graph-gallery.com/graph/parallel_basic.html
-  // Extract the list of dimensions we want to keep in the plot. Keep all.
-  dimensions = Object.keys(data[0]).filter(function(d) { return (d != "job_title" && d != "employee_residence" && d != "company_location") });
+  // Extract the list of dimensions we want to keep in the plot. Keep all except the categoricals with too many unique values (>50).
+  dimensions = Object.keys(data[0]).filter(function (d) {
+    return (d != "job_title" &&
+      d != "employee_residence" && d != "company_location" && d != "salary")
+  });
   
   // For each dimension, build a linear scale if numerical, else ordinal scale. Store all in a y object
-  var y = {}
+  var y1 = {}
   for (i in dimensions) {
     dim = dimensions[i]
     if (dim != 'salary' && dim != 'salary_in_usd') {
       const uniqueDimVals = [...new Set(data.map(item => item[dim]))];
       console.log(uniqueDimVals)
-      y[dim] = d3.scalePoint()
+      y1[dim] = d3.scalePoint()
         .domain( uniqueDimVals )
-        .range([height, 0])
+        .range([parallelHeight, 0])
     }    
     else {
-      y[dim] = d3.scaleLinear()
-        .domain( d3.extent(data, function(d) { return d[dim]; }) )
-        .range([height, 0])
+      y1[dim] = d3.scaleLinear()
+        .domain( d3.extent(data, function(d) { return +d[dim]; }) )
+        .range([parallelHeight, 0])
     }
   }
 
   // Build the X scale -> it find the best position for each Y axis
   x = d3.scalePoint()
-    .range([0, width])
+    .range([0, parallelWidth])
     .padding(1)
     .domain(dimensions);
 
   // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
   function path(d) {
-      return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+      return d3.line()(dimensions.map(function(p) { return [x(p), y1[p](d[p])]; }));
   }
-  const svg = d3.select("svg")
+  
 
   // Draw the lines
-  svg
+  g1
     .selectAll("myPath")
     .data(data)
     .join("path")
@@ -92,14 +75,14 @@ d3.csv("ds_salaries.csv").then(data => {
     .style("opacity", 0.5)
 
   // Draw the axis:
-  svg.selectAll("myAxis")
+  g1.selectAll("myAxis")
     // For each dimension of the dataset I add a 'g' element:
     .data(dimensions).enter()
     .append("g")
     // I translate this element to its right position on the x axis
     .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
     // And I build the axis with the call function
-    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y1[d])); })
     // Add axis title
     .append("text")
       .style("text-anchor", "middle")
@@ -107,6 +90,13 @@ d3.csv("ds_salaries.csv").then(data => {
       .text(function(d) { return d; })
       .style("fill", "black")
   
+  g1.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", -25)
+    .attr("text-anchor", "middle")  
+    .style("font-size", "14px") 
+    .style("text-decoration", "underline")  
+    .text("Annual Data Scientist Salaries");
   
 }).catch(function(error){
   console.log(error);
