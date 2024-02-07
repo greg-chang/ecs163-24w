314d15fa0b649592ae16bb597259ed57e6fdc123
Kris Wong <37485546+SerpentSnek@users.noreply.github.com>
2024-02-07 15:38:25 -0800

    Update main.js
    we done

--- Homework2/ykkwong/main.js
+++ Homework2/ykkwong/main.js
@@ -7,15 +7,15 @@ let parallelMargin = {top: 40, right: 0, bottom: 10, left: 0},
     parallelWidth = width,
     parallelHeight = 350 - parallelMargin.top;
 
-// let distrLeft = 400, distrTop = 0;
-// let distrMargin = {top: parallelHeight, right: 30, bottom: 30, left: 30},
-//     distrWidth = width/2,
-//     distrHeight = 350 - parallelHeight;
+let barLeft = 130, barTop = 450 + parallelHeight;
+let barMargin = {top: parallelHeight, right: 30, bottom: 30, left: 30},
+    barWidth = width/2 - 200,
+    barHeight = 350 - height - parallelHeight;
 
-let teamLeft = 130, teamTop = 450 + parallelHeight;
-let teamMargin = {top: parallelHeight, right: 30, bottom: 30, left: 30},
-    teamWidth = width/2,
-    teamHeight = 350 - height - parallelHeight;
+let lineLeft = barLeft + 100, lineTop = 40;
+let lineMargin = {top: parallelHeight, right: 30, bottom: 30, left: 30},
+    lineWidth = width/2 - 200,
+    lineHeight = 350 - height - parallelHeight;
 
 // get the data
 d3.csv("ds_salaries.csv").then(data => {
@@ -96,14 +96,14 @@ d3.csv("ds_salaries.csv").then(data => {
     .attr("text-anchor", "middle")  
     .style("font-size", "14px") 
     .style("text-decoration", "underline")  
-    .text("Annual Data Scientist Salaries");
+    .text("Overview of Data Scientist Work Aspects");
   
   // ----------------------------------------------------------------------------------
   // bar graph: average salary per experience level            
   const g2 = svg.append("g")
-              .attr("width", teamWidth + teamMargin.left + teamMargin.right)
-              .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
-              .attr("transform", `translate(${teamMargin.left}, ${teamTop})`)
+              .attr("width", barWidth + barMargin.left + barMargin.right)
+              .attr("height", barHeight + barMargin.top + barMargin.bottom)
+              .attr("transform", `translate(${barMargin.left}, ${barTop})`)
   
   function groupBy(objectArray, property) {
     return objectArray.reduce(function (acc, obj) {
@@ -131,14 +131,15 @@ d3.csv("ds_salaries.csv").then(data => {
     formattedData.push(processedObj);
   });
   
-  let y_axis_start = (teamHeight+teamMargin.top+230);
+  let y_axis_start = (barHeight+barMargin.top+230);
 
   // X axis
   var x = d3.scaleLinear()
     .domain([0, 200000])
-    .range([ 0, width/3]);
+    .range([0, barWidth]);
+  
   g2.append("g")
-    .attr("transform", "translate(" + teamLeft + ',' + (teamHeight+teamMargin.top+230) + ")")
+    .attr("transform", "translate(" + barLeft + ',' + (barHeight+barMargin.top+230) + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
@@ -146,15 +147,14 @@ d3.csv("ds_salaries.csv").then(data => {
       
   // Y axis
   var y = d3.scaleBand()
-    .range([ 0, teamHeight/3 ])
+    .range([ 0, barHeight/3 ])
     .domain([...new Set(data.map(item => item['experience_level']))])
     .padding(0.05);
+  
   g2.append("g")
     .call(d3.axisLeft(y))
-    .attr("transform", "translate(" + teamLeft + ',' + y_axis_start + ")")
-
+    .attr("transform", "translate(" + barLeft + ',' + y_axis_start + ")")
   
-
   g2
     .selectAll("rect") 
     .data(formattedData)
@@ -164,19 +164,69 @@ d3.csv("ds_salaries.csv").then(data => {
     .attr("width", d => x(d.average))
     .attr("height", y.bandwidth())
     .attr("fill", "#69b3a2")
-    .attr("transform", "translate(" + teamLeft + ',' + y_axis_start + ")")
+    .attr("transform", "translate(" + barLeft + ',' + y_axis_start + ")")
 
   g2.append("text")
-    .attr("transform", "translate(" + (teamLeft + teamMargin.left + 150) + ',' + (teamHeight + parallelHeight) + ")")
+    .attr("transform", "translate(" + (barLeft + barMargin.left + 150) + ',' + (barHeight + parallelHeight) + ")")
     .attr("text-anchor", "middle")  
     .style("font-size", "14px") 
     .style("text-decoration", "underline")  
     .text("Average Salary in USD by Experience Level");
   
   //-----------------------------------------------------------------------------------
+  // line chart year vs average salary
+  const g3 = svg.append("g")
+              .attr("width", lineWidth + lineMargin.left + lineMargin.right)
+              .attr("height", lineHeight + lineMargin.top + lineMargin.bottom)
+              .attr("transform", `translate(${lineMargin.left + barWidth + 200}, ${lineTop})`)
+              
+  let years = groupBy(data, "work_year");
+  formattedData = [];
+  Object.keys(years).forEach(d => {
+    let initialValue = 0;
+    let cumulativeSalary = years[d].reduce((acc, curvalue) => acc + Number(curvalue.salary_in_usd), initialValue);
+    let average = cumulativeSalary / years[d].length ;
+    
+    let processedObj = {
+      year: d,
+      average: average
+    }
+    formattedData.push(processedObj);
+  });
   
-
-
+  x = d3.scalePoint()
+      .domain([...new Set(data.map(item => item['work_year']))].sort())
+      .range([ lineMargin.left, lineWidth ]);
+  g3.append("g")
+    .attr("transform", "translate(0," + (height/2 + 255) + ")")
+    .call(d3.axisBottom(x));
+      
+  y = d3.scaleLinear()
+    .domain( [90000, 160000])
+    .range([ height-100, height/2 ]);
+  g3.append("g")
+    .call(d3.axisLeft(y));
+  
+  // Add the line
+  g3.append("path")
+    .datum(formattedData)
+    .attr("fill", "none")
+    .attr("stroke", "#69b3a2")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(d => x(d.year))
+      .y(d => y(d.average))
+      )
+  // Add the points
+  g3
+    .append("g")
+    .selectAll("dot")
+    .data(formattedData)
+    .join("circle")
+      .attr("cx", d => x(d.year) )
+      .attr("cy", d => y(d.average)) 
+      .attr("r", 5)
+      .attr("fill", "#69b3a2")
   
   
 }).catch(function(error){
