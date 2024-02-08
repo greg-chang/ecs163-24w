c09cfd5a0c290bea9fec01e34ef1d26adb4d35a8
acnieto707 <alfredonieto707@gmail.com>
2024-02-07 21:21:26 -0800

    Finished 2/3 Visualizations
    Just need advanced vis

--- Homework2/acnieto/main.js
+++ Homework2/acnieto/main.js
@@ -1,31 +1,22 @@
-let abFilter = 0
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 let scatterLeft = 0, scatterTop = 0;
-let scatterMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    scatterWidth = 400 - scatterMargin.left - scatterMargin.right,
-    scatterHeight = 350 - scatterMargin.top - scatterMargin.bottom;
+let scatterMargin = {top: 10, right: 10, bottom: 10, left: 65},
+    scatterWidth = ((2 * width)/5) - scatterMargin.left - scatterMargin.right,
+    scatterHeight = 250 - scatterMargin.top - scatterMargin.bottom;
 
-let distrLeft = 400, distrTop = 0;
-let distrMargin = {top: 20, right: 30, bottom: 10, left: 60},
-    distrWidth = 400 - distrMargin.left - distrMargin.right,
-    distrHeight = 350 - distrMargin.top - distrMargin.bottom;
-
-let jobsLeft = 0, jobsTop = 400;
-let jobsMargin = {top: 10, right: 30, bottom: 40, left: 60},
+let jobsLeft = 0, jobsTop = 350;
+let jobsMargin = {top: 10, right: 30, bottom: 10, left: 65},
     jobsWidth = width - jobsMargin.left - jobsMargin.right,
-    jobsHeight = height-450 - jobsMargin.top - jobsMargin.bottom;
+    jobsHeight = height - 435 - jobsMargin.top - jobsMargin.bottom;
 
 
 d3.csv("ds_salaries.csv").then(rawData =>{
     console.log("rawData", rawData);
     
     rawData.forEach(function(d){
-        d.AB = Number(d.AB);
-        d.H = Number(d.H);
         d.salary_in_usd = Number(d.salary_in_usd);
-        d.SO = Number(d.SO);
         d.remote_ratio = Number(d.remote_ratio);
     });
     
@@ -34,38 +25,44 @@ d3.csv("ds_salaries.csv").then(rawData =>{
     rawData = rawData.map(d=>{
                           return {
                               "salary_in_usd":d.salary_in_usd,
-                              "SO_AB":d.SO/d.AB,
                               "job_title":d.job_title,
-                              "remote_ratio":d.remote_ratio
+                              "remote_ratio":d.remote_ratio,
+                              "company_location":d.company_location
                           };
     });
     console.log(rawData);
-    
-//plot 1
-    const svg = d3.select("svg")
 
+    q = rawData.reduce((s, { job_title }) => (s[job_title] = (s[job_title] || 0) + 1, s), {});
+    r = Object.keys(q).map((key) => ({ job_title: key, count: q[key] }));
+    r = r.filter(d=> d.count>15);
+    console.log(r);
+    
+// Plot 1 - Salary(USD) vs Ratio of Remote
+    const svg = d3.select("svg")
+    
     const g1 = svg.append("g")
                 .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
                 .attr("height", scatterHeight + scatterMargin.top + scatterMargin.bottom)
                 .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`)
+                .attr("style", "outline: thin solid red;")   // Test Border
 
     // X label
     g1.append("text")
     .attr("x", scatterWidth / 2)
-    .attr("y", scatterHeight + 50)
+    .attr("y", scatterHeight + 60)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("H/AB")
+    .text("Salary (USD)")
     
 
     // Y label
     g1.append("text")
-    .attr("x", -(scatterHeight / 2))
+    .attr("x", -(scatterHeight / 2 + 40))
     .attr("y", -40)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
-    .text("SO/AB")
+    .text("Ratio of Remote")
 
     // X ticks
     const x1 = d3.scaleLinear()
@@ -86,10 +83,10 @@ d3.csv("ds_salaries.csv").then(rawData =>{
     // Y ticks
     const y1 = d3.scaleLinear()
     .domain([0, d3.max(rawData, d => d.remote_ratio)])
-    .range([scatterHeight, 0])
+    .range([scatterHeight, 30])
 
     const yAxisCall = d3.axisLeft(y1)
-                        .ticks(13)
+                        .ticks(3)
     g1.append("g").call(yAxisCall)
 
     const rects = g1.selectAll("circle").data(rawData)
@@ -101,37 +98,31 @@ d3.csv("ds_salaries.csv").then(rawData =>{
          .attr("cy", function(d){
              return y1(d.remote_ratio);
          })
-         .attr("r", 3)
-         .attr("fill", "#69b3a2")
+         .attr("r", 5)
+         .attr("fill", "black")
 
-//space
-    const g2 = svg.append("g")
-                .attr("width", distrWidth + distrMargin.left + distrMargin.right)
-                .attr("height", distrHeight + distrMargin.top + distrMargin.bottom)
-                .attr("transform", `translate(${distrLeft}, ${distrTop})`)
+// Plot 2
 
-//plot 2
-    
-    q = rawData.reduce((s, { job_title }) => (s[job_title] = (s[job_title] || 0) + 1, s), {});
-    r = Object.keys(q).map((key) => ({ job_title: key, count: q[key] }));
-    r = r.filter(d=> d.count>35);
-    console.log(r);
 
+
+// Plot 3 - Overview
            
     const g3 = svg.append("g")
                 .attr("width", jobsWidth + jobsMargin.left + jobsMargin.right)
                 .attr("height", jobsHeight + jobsMargin.top + jobsMargin.bottom)
                 .attr("transform", `translate(${jobsMargin.left}, ${jobsTop})`)
+                .attr("border", 1)
+                .attr("style", "outline: thin solid red;")   // Test Border
 
     // X label
-    /*
+    
     g3.append("text")
     .attr("x", jobsWidth / 2)
-    .attr("y", jobsHeight + 50)
-    .attr("font-size", "20px")
+    .attr("y", jobsHeight + 90)
+    .attr("font-size", "24px")
     .attr("text-anchor", "middle")
     .text("Job Title")
-    */
+    
 
     // Y label
     g3.append("text")
@@ -157,7 +148,7 @@ d3.csv("ds_salaries.csv").then(rawData =>{
         .attr("y", "10")
         .attr("x", "-5")
         .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)")
+        .attr("transform", "rotate(-35)")
 
     // Y ticks
     const y2 = d3.scaleLinear()
@@ -175,7 +166,7 @@ d3.csv("ds_salaries.csv").then(rawData =>{
     .attr("x", (d) => x2(d.job_title))
     .attr("width", x2.bandwidth)
     .attr("height", d => jobsHeight - y2(d.count))
-    .attr("fill", "grey")
+    .attr("fill", "orange")
 
 
 
