0c8f83e61df77dbb660ef9a10c416329d5d19c59
Anh Nguyen <anmnguy@ucdavis.edu>
2024-02-09 15:28:07 -0800

    new d3

--- Homework2/anmnguy/d3.js
+++ Homework2/anmnguy/d3.js
@@ -1,18 +1,14 @@
+
 // Load the CSV data
 d3.csv("studentMentalHealth.csv").then(rawData => {
     console.log(rawData);
 
-    rawData.forEach(function(d) {
-        d["Do you have Depression?"] = d["Do you have Depression?"] === "Yes" ? 1 : 0;
-        d["Do you have Anxiety?"] = d["Do you have Anxiety?"] === "Yes" ? 1 : 0;
-        d["Do you have Panic attack?"] = d["Do you have Panic attack?"] === "Yes" ? 1 : 0;
-    });
     const genderCounts = d3.rollup(rawData, v => v.length, d => d["Choose your gender"]);
 
+    const svg = d3.select("svg")
     // Select the existing SVG and append a 'g' element for the pie chart
-    const svg = d3.select("svg").append("g")
-        .attr("transform", "translate(200,200)"); // Center the pie chart
-
+    const g1 = svg.append("g")
+                .attr("transform", "translate(300,200)")
     const pie = d3.pie().value(d => d[1]);
     const data_ready = pie(Array.from(genderCounts));
 
@@ -23,7 +19,7 @@ d3.csv("studentMentalHealth.csv").then(rawData => {
         .range(["#6f8ab1", "#f0ab8d"]); // Assigning colors
 
     // Append slices to the pie chart
-    svg.selectAll('path')
+    g1.selectAll('path')
         .data(data_ready)
         .enter()
         .append('path')
@@ -34,28 +30,28 @@ d3.csv("studentMentalHealth.csv").then(rawData => {
         .style("opacity", 0.7);
 
     // Add title
-    svg.append("text")
+    g1.append("text")
        .attr("x", 0) 
-       .attr("y", -130) // Adjust based on your SVG's layout
+       .attr("y", -150) // Adjust based on your SVG's layout
        .attr("text-anchor", "middle")
        .style("font-size", "18px")
        .text("Gender Distribution");
 
     // Add legend
-    const legend = svg.selectAll(".legend")
+    const legend = g1.selectAll(".legend")
        .data(color.domain())
        .enter().append("g")
        .attr("class", "legend")
        .attr("transform", (d, i) => "translate(0," + (i * 20 - 100) + ")"); // Adjust for layout
 
     legend.append("rect")
-       .attr("x", -180) // Adjust based on your SVG's layout
+       .attr("x", -200) // Adjust based on your SVG's layout
        .attr("width", 18)
        .attr("height", 18)
        .style("fill", color);
 
     legend.append("text")
-       .attr("x", -155) // Adjust based on positioning
+       .attr("x", -175) // Adjust based on positioning
        .attr("y", 9)
        .attr("dy", ".35em")
        .style("text-anchor", "start")
