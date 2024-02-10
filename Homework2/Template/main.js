c305b64f6f9f4d7b469b22cce4f08c0724822185
Orpheus-1 <chiyou.xiong@yahoo.com>
2024-02-10 01:00:08 -0800

    complete

--- Homework2/Template/main.js
+++ Homework2/Template/main.js
@@ -173,34 +173,6 @@ d3.csv("players.csv").then(rawData =>{
     .attr("fill", "grey")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }).catch(function(error){
     console.log(error);
 });
